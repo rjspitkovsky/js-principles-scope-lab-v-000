779d31479cd653103fc1e85767a2296e057618ba
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-01 22:27:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution in this file!
-var customerName = 'bob';
+
 
 function upperCaseCustomerName() {
-
+  var customerName = 'bob'
     const uppercase = customerName.toUpperCase()
     return uppercase
 }
